feaba77cc9c56eefba33842451fc0fe3292446bf
stormstina <stina.nerin@gmail.com>
2023-11-02 20:24:35 +0100

    Add applicaiton routing & pages setup

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProductGalleryPageComponent } from './pages/product-gallery-page/product-gallery-page.component';
+import { ProductDetailPageComponent } from './pages/product-detail-page/product-detail-page.component';
+import { CheckoutPageComponent } from './pages/checkout-page/checkout-page.component';
+import { NotFoundPageComponent } from './pages/not-found-page/not-found-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductGalleryPageComponent,
+    ProductDetailPageComponent,
+    CheckoutPageComponent,
+    NotFoundPageComponent
   ],
   imports: [
     BrowserModule,
