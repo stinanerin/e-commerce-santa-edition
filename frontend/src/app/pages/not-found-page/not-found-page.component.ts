3076b21c7508b81475da4a8f537b70aabbf6f86b
stormstina <stina.nerin@gmail.com>
2023-11-08 15:27:31 +0100

    Add btn text variable to not found page

--- frontend/src/app/pages/not-found-page/not-found-page.component.ts
+++ frontend/src/app/pages/not-found-page/not-found-page.component.ts
@@ -9,5 +9,6 @@ import { Component, Input } from '@angular/core';
 export class NotFoundPageComponent {
 
   @Input() notFoundMessage: string = " we were unable to find the page you were looking for."
+  @Input() CTA: string = " Go back to the homepage."
 
 }
