799de8bc7e7b002d2d2aa4eb000bf865d57b194e
stormstina <stina.nerin@gmail.com>
2023-11-08 15:05:00 +0100

    Add test file with tests for not-found page

--- frontend/src/app/pages/not-found-page/not-found-page.component.ts
+++ frontend/src/app/pages/not-found-page/not-found-page.component.ts
@@ -5,6 +5,7 @@ import { Component } from '@angular/core';
   templateUrl: './not-found-page.component.html',
   styleUrls: ['./not-found-page.component.scss']
 })
+
 export class NotFoundPageComponent {
 
 }
