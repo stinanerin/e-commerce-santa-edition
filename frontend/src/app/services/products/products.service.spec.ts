8706db1c2b007990f467fef9b3ea14cd7d626137
stormstina <stina.nerin@gmail.com>
2023-11-10 17:59:56 +0100

    Update service test

--- frontend/src/app/services/products/products.service.spec.ts
+++ frontend/src/app/services/products/products.service.spec.ts
@@ -1,52 +1,53 @@
 import { TestBed } from '@angular/core/testing';
-import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { ProductsService } from './products.service';
-import { PRODUCTS } from "../../data/mock-products";
-import { Product } from 'src/app/models/product';
+import { environment } from '../../../environments/environment';
+import { PRODUCTS } from 'src/app/data/mock-products';
 
 describe('ProductsService', () => {
   let service: ProductsService;
+  let httpTestingController: HttpTestingController;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
       imports: [HttpClientTestingModule],
       providers: [ProductsService],
     });
+
     service = TestBed.inject(ProductsService);
+    httpTestingController = TestBed.inject(HttpTestingController);
   });
 
-  it('service created', () => {
+  afterEach(() => {
+    httpTestingController.verify();
+  });
+
+  it('should be created', () => {
     expect(service).toBeTruthy();
   });
 
-  it('should return all products', (done) => {
-    service.getAllProducts().subscribe((products) => {
-      expect(products).toEqual(PRODUCTS);
-      done();
+  describe('getAllProducts', () => {
+    it('should return mock products if useApi is false', () => {
+      Object.defineProperty(environment, 'useApi', { get: () => false });
+
+      service.getAllProducts().subscribe((data) => {
+        expect(data).toEqual(PRODUCTS);
+      });
     });
-  });
 
-  it('should return a product by id', (done) => {
-    // From mockdata
-    const productId = 1; 
-    
-    service.getProductById(productId).subscribe((product) => {
-      const foundProduct:Product | undefined = PRODUCTS.find((p) => p.id === productId);
-      
-      expect(foundProduct).toBeDefined(); 
+    it('should make a GET request to the API if useApi is true', () => {
+      Object.defineProperty(environment, 'useApi', { get: () => true });
 
-      if (foundProduct) {
-        expect(product).toEqual(foundProduct);
-      }
-      done();
+      const mockResponse = { data: PRODUCTS, statusCode: 200 };
+
+      service.getAllProducts().subscribe((data) => {
+        expect(data).toEqual(PRODUCTS);
+      });
+
+      const req = httpTestingController.expectOne(`${service.baseUrl}/products`);
+      expect(req.request.method).toEqual('GET');
+      req.flush(mockResponse);
     });
-  });
 
-  it('should throw an error if product is not found', (done) => {
-    const invalidProductId = 1000; // Replace with a non-existing product id
-    expect(() => {
-      service.getProductById(invalidProductId).subscribe();
-    }).toThrowError(`Product with id ${invalidProductId} not found`);
-    done();
   });
 });
